ee89d3110d1f02648122ecb2f970f3add937b59e
mehdihadeli <mehdi.hadeli@outlook.com>
2022-07-18 18:36:14 +0430

    add first solution

---
+++ src/LeetCodeSolutions/Difficulty/Easy/1.two-sum.cs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=1 lang=csharp
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (45.39%)
+ * Likes:    14924
+ * Dislikes: 540
+ * Total Accepted:    2.9M
+ * Total Submissions: 6.4M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ *
+ * Example:
+ *
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ *
+ */
+
+
+// @lc code=start
+namespace LeetCodeSolutions.Difficulty.Easy;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>(nums.Length);
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var curr = nums[i];
+            if (map.ContainsKey(curr))
+            {
+                return new int[] { map[curr], i };
+            }
+            map[target - nums[i]] = i;
+        }
+        return null;
+    }
+}
+// @lc code=end
---
+++ src/Leetcode.Tests/Difficulty/Easy/1.two-sum.Test.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Leetcode.Tests.Difficulty.Easy;
+
+using FluentAssertions;
+using LeetCodeSolutions.Difficulty.Easy;
+using Xunit;
+
+public class TwoSumTest
+{
+    [Fact]
+    public void Test()
+    {
+        var res = new Solution().TwoSum(new[] { 2, 7, 11, 15 }, 9);
+        var expected = new[] { 0, 1 };
+        res.Should().Equal(expected);
+    }
+}
